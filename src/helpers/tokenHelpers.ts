1107f2862a21bd1361ba3150754f4a003f8a3bd0
amokrzycki <amokrzycki@proton.me>
2026-09-02 09:05:29 +0200

    Fix: Remember me

--- src/helpers/tokenHelpers.ts
+++ src/helpers/tokenHelpers.ts
@@ -4,11 +4,23 @@
  * Refresh tokens are stored in httpOnly cookies (secure, not accessible to JavaScript)
  */
 
+const SESSION_KEY = "authenticated";
+
+export const rememberSession = (rememberMe: boolean) => {
+  sessionStorage.setItem(SESSION_KEY, "true");
+  if (rememberMe) localStorage.setItem("rememberMe", "true");
+  else localStorage.removeItem("rememberMe");
+};
+
+export const hasStoredSession = () =>
+  sessionStorage.getItem(SESSION_KEY) === "true" || localStorage.getItem("rememberMe") === "true";
+
 export const clearTokenStorage = () => {
   // Clear legacy auth artifacts (tokens/user) and non-sensitive flags
   localStorage.removeItem("accessToken");
   localStorage.removeItem("token");
   localStorage.removeItem("user");
   localStorage.removeItem("rememberMe");
+  sessionStorage.removeItem(SESSION_KEY);
   // Refresh token cookie is cleared by backend on logout
 };
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { persistor, store } from "./store/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
 import { accountsApiSlice } from "./components/Accounts/accountsApiSlice.ts";
 import { loginUser } from "./components/Accounts/accountSlice.ts";
+import { clearTokenStorage, hasStoredSession } from "./helpers/tokenHelpers.ts";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Root element not found");
@@ -19,10 +20,12 @@ ReactDOM.createRoot(rootElement).render(
   </React.StrictMode>,
 );
 
-void store
-  .dispatch(accountsApiSlice.endpoints.refreshSession.initiate())
-  .unwrap()
-  .then(({ access_token, user }) => {
-    store.dispatch(loginUser({ accessToken: access_token, user }));
-  })
-  .catch(() => undefined);
+if (hasStoredSession()) {
+  void store
+    .dispatch(accountsApiSlice.endpoints.refreshSession.initiate())
+    .unwrap()
+    .then(({ access_token, user }) => {
+      store.dispatch(loginUser({ accessToken: access_token, user }));
+    })
+    .catch(clearTokenStorage);
+}
